c523017c4e670898468d18af6424f2f308f04a37
dhy257 <js.dhyeo@gmail.com>
2025-02-19 18:36:09 +0900

    feat:화면 연결 수정
    로그인, 회원가입 연결 조정

--- frontend/app/src/main/java/com/example/frontend/FreshCheckApp.kt
+++ frontend/app/src/main/java/com/example/frontend/FreshCheckApp.kt
@@ -1,34 +1,27 @@
 package com.example.frontend
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-//
-//enum class composetest(){
-//    LoginScreen,
-//    Register
-//}
+
 
 @Composable
-fun FreshCheckApp(){
+fun FreshCheckApp() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = Routes.LoginScreen, builder = {
-        composable(Routes.LoginScreen,){
-            LoginScreen(navController)
-        }
-        composable(Routes.RegisterScreen){
-            RegisterScreen(navController)
-        }
-    })
-}
+    val context = LocalContext.current  // Context 가져오기
+    NavHost(navController = navController,
+        startDestination = Routes.LoginScreen,
+        builder = {
+            //로그인 화면
+            composable(Routes.LoginScreen) {
+                LoginScreen(navController, context)
+            }
+            //회원가입 화면
+            composable(Routes.RegisterScreen) {
+                RegisterScreen(navController, context)
+            }
 
-//val navController = rememberNavController()
-//NavHost(navController = navController, startDestination = Routes.LoginScreen, builder = {
-//    composable(Routes.LoginScreen,){
-//        LoginScreen(navController)
-//    }
-//    composable(Routes.RegisterScreen){
-//        RegisterScreen()
-//    }
-//})
+        })
+}
